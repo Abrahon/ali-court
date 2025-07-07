6965efbb497311051ba2f3af0691b7e63b620b90
Sujon <mdabrahon926@gmail.com>
2025-07-07 19:48:59 +0600

    updated allowed host

--- alicourt/settings.py
+++ alicourt/settings.py
@@ -27,9 +27,9 @@ STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/']
+# ALLOWED_HOSTS = ['http://127.0.0.1:8000/']
 
-# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
